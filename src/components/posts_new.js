cf0b92a30a92c3f656fa36c6bc6760d84268ad3e
somil kumar <somil.kumar91@gmail.com>
2017-11-01 23:25:19 +0530

    Adding delete and show post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+import { createPost } from '../actions/index';
+
 class PostsNew extends Component {
     renderField(field) {
         const { meta: { touched, error} } = field;
@@ -22,7 +25,10 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+       
+       this.props.createPost(values, () => {
+        this.props.history.push('/'); 
+       });
     }
 
 
@@ -74,8 +80,12 @@ function validate(values) {
 }
 
 
+
+
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+   connect(null, { createPost })(PostsNew)
+);
 
